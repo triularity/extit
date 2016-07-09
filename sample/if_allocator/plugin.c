d816cd6c3a47a7e14432ac6c6c849a9b6def0e5b
Chad Fraleigh <chadf@triularity.org>
2016-07-09 00:28:25 +0000

    Added sample code for allocator interface.

---
+++ sample/if_allocator/plugin.c
@@ -0,0 +1,127 @@
+/*
+ * @(#) sample/if_allocator/plugin.c
+ *
+ * This file is in the Public Domain.
+ */
+
+#include <iv/base.h>
+#include <iv/util.h>
+#include <extit/base.h>
+#include <extit/plugin.h>
+#include <extit/plugin_spi.h>
+#include <extit/if/allocator.h>
+
+
+typedef struct _myplugin
+{
+	extit_if_allocator_t *	allocator;
+	char *			buffer;
+} myplugin_t;
+
+
+static
+extit_status_t
+EXTIT_DECL
+plugin_handler
+(
+	iv_version_t abi_version,
+	const extit_container_t *container,
+	unsigned int cmd,
+	void *param,
+	unsigned int flags
+)
+{
+	extit_if_allocator_t *		allocator;
+	myplugin_t *			myplugin;
+	extit_spi_param_create_t *	param_create;
+	extit_spi_param_destroy_t *	param_destroy;
+
+
+	switch(cmd)
+	{
+		case EXTIT_SPI_CMD_PROBE:
+			/*
+			 * We only need to support v1.0 for what we do
+			 */
+			if(!iv_matches(abi_version, EXTIT_ABI_VERSION_1_0))
+				return EXTIT_STATUS_UNSUPPORTED;
+
+			/*
+			 * Verify allocator support
+			 */
+			if(extit_container_query_interface(
+			 container,
+			 EXTIT_IF_ALLOCATOR_IID,
+			 EXTIT_IF_ALLOCATOR_ABI_1_0)
+			  == IV_VERSION_NONE)
+			{
+				extit_container_log(
+					container,
+					"Allocator interface 1.0 required");
+
+				return EXTIT_STATUS_FAIL;
+			}
+
+			/*
+			 * No need to check abi_version for other commands
+			 * since they will only be called if v1.0+ after this
+			 */
+			return EXTIT_STATUS_OK;
+
+		case EXTIT_SPI_CMD_CREATE:
+			extit_container_log(container, "Creating plugin\n");
+
+			param_create = (extit_spi_param_create_t *) param;
+
+			/*
+			 * Get allocator
+			 */
+			allocator = extit_container_get_interface(
+				container,
+				EXTIT_IF_ALLOCATOR_IID,
+				EXTIT_IF_ALLOCATOR_ABI_1_0);
+
+			/*
+			 * Use allocator to create plugin
+			 */
+			myplugin = extit_if_allocator_alloc(
+				allocator, sizeof(myplugin_t));
+
+			myplugin->allocator = allocator;
+
+			/*
+			 * And some data buffer
+			 */
+			myplugin->buffer = extit_if_allocator_alloc(
+				allocator, 1000);
+
+			if(myplugin->buffer == NULL)
+			{
+				extit_if_allocator_free(allocator, myplugin);
+				return EXTIT_STATUS_NOMEM;
+			}
+
+			param_create->spi_ctx = myplugin;
+
+			return EXTIT_STATUS_OK;
+
+		case EXTIT_SPI_CMD_DESTROY:
+			extit_container_log(container, "Destroying plugin\n");
+
+			param_destroy = (extit_spi_param_destroy_t *) param;
+
+			myplugin = (myplugin_t *) param_destroy->spi_ctx;
+			allocator = myplugin->allocator;
+
+			extit_if_allocator_free(allocator, myplugin->buffer);
+			extit_if_allocator_free(allocator, myplugin);
+
+			return EXTIT_STATUS_OK;
+
+		default:
+			return EXTIT_STATUS_NOTIMPLEMENTED;
+	}
+}
+
+EXTIT_DECLARE_SPI("{b61bf752-4562-11e6-9660-406186e454c1}", IV_VERSION(1,0), "Allocating Plugin", "1.0", plugin_handler)
+
---
+++ sample/if_allocator/server.c
@@ -0,0 +1,114 @@
+/*
+ * @(#) sample/if_allocator/server.c
+ *
+ * This file is in the Public Domain.
+ */
+
+#include <stdio.h>
+#include <string.h>
+
+#include <iv/base.h>
+#include <extit/container.h>
+#include <extit/if/allocator.h>
+#include <extit/if/allocator_stdimpl.h>
+
+
+static
+void *
+EXTIT_DECL
+my_get_interface
+(
+	const extit_container_t *container,
+	const char *id,
+	iv_version_t version
+)
+{
+	if((strcmp(id, EXTIT_IF_ALLOCATOR_IID) == 0)
+	 && (version == EXTIT_IF_ALLOCATOR_ABI_1_0))
+	{
+		return &extit_if_allocator_stdimpl_1_0;
+	}
+
+	return NULL;
+}
+
+
+static
+iv_version_t
+EXTIT_DECL
+my_query_interface
+(
+	const extit_container_t *container,
+	const char *id,
+	iv_version_t base_version
+)
+{
+	if((strcmp(id, EXTIT_IF_ALLOCATOR_IID) == 0)
+	 && (base_version == EXTIT_IF_ALLOCATOR_ABI_1_0))
+	{
+		return EXTIT_IF_ALLOCATOR_ABI_1_0;
+	}
+
+	return IV_VERSION_NONE;
+}
+
+
+static
+extit_container_ops_1_0_t	my_container_ops =
+{
+	extit_container_get_symbol_default,		/* get_symbol */
+	extit_container_get_function_default,		/* get_function */
+	my_get_interface,				/* get_interface */
+	my_query_interface,				/* query_interface */
+	extit_container_log_default			/* log */
+};
+
+static
+extit_container_t		my_container =
+{
+	EXTIT_ABI_VERSION_1_0,				/* version */
+	&my_container_ops				/* ops */
+};
+
+
+
+int
+main(int argc, char **argv)
+{
+	extit_module_t *	module;
+	extit_plugin_t *	plugin;
+
+
+	if(argc != 2)
+	{
+		fprintf(stderr, "Usage: if_allocator_server <plugin-path>\n");
+		return 1;
+	}
+
+	module = extit_module_load(
+			&my_container,
+			argv[1],
+			EXTIT_FLAG_LOG_TRACE);
+
+	if(module != NULL)
+	{
+		plugin = extit_module_create_plugin(module);
+
+		if(plugin != NULL)
+		{
+			extit_plugin_destroy(plugin);
+		}
+		else
+		{
+			fprintf(stderr, "Unable to create plugin\n");
+		}
+
+		extit_module_release(module);
+	}
+	else
+	{
+		fprintf(stderr, "Unable to load module\n");
+	}
+
+	return 0;
+}
